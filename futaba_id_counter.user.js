57766b053efcc59f6264546baacfc5c532fe8556
toshiakisp <toshiaki.sp@mail>
2013-07-03 23:34:32 +0900

    Add @grant metadata for Greacemonkey

--- futaba_id_counter.user.js
+++ futaba_id_counter.user.js
@@ -5,6 +5,7 @@
 // @namespace      https://github.com/toshiakisp
 // @author         toshiaki.sp
 // @license        MIT
+// @grant          none
 // @downloadURL    https://github.com/toshiakisp/idcounter-userscript/raw/master/futaba_id_counter.user.js
 // @include        http://*.2chan.net/*/res/*.htm
 // @include        unmht://www.nijibox2.com/http.5/futalog/*/src/*.mht/
